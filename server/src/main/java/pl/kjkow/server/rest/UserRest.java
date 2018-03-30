f6fdcd60912ba611dafde1adb40bb058cd7d1e14
kamil.kowalczyk <karmel92@o2.pl>
2018-03-30 13:52:43 +0200

    add simple user create

--- server/src/main/java/pl/kjkow/server/rest/UserRest.java
+++ server/src/main/java/pl/kjkow/server/rest/UserRest.java
@@ -2,6 +2,7 @@ package pl.kjkow.server.rest;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 import pl.kjkow.server.model.User;
@@ -19,5 +20,10 @@ public class UserRest {
     @GetMapping("/all")
     public @ResponseBody Iterable<User> getAllUsers(){
         return userRepository.findAll();
+    }
+
+    @RequestMapping("/add")
+    public @ResponseBody User addUser(String email, String name, String lastName){
+        return userRepository.save(new User(name, lastName, email));
     }
 }
