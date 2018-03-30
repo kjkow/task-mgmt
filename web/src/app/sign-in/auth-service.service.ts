efa5ebc094eaa6b21c286b9a7720533d4cf0ab74
kamil.kowalczyk <karmel92@o2.pl>
2018-03-30 08:29:10 +0200

    logowanie do google z uzyciem angular4-social

--- web/src/app/sign-in/auth-service.service.ts
+++ web/src/app/sign-in/auth-service.service.ts
@@ -1,123 +1,29 @@
 import { Injectable } from '@angular/core';
-import { HttpHeaders } from '@angular/common/http';
-import { HttpClient } from '@angular/common/http';
-import { Response } from '@angular/http';
-import 'rxjs/add/operator/map';
-
 
 @Injectable()
-export class AuthService {
+export class UsersService {
 
-  constructor(private http:HttpClient ) { }
+  constructor() { }
 
-  getToken(){
-    console.log("start getToken()");
-
-    var code = localStorage.getItem('auth_code');
-    var auth = localStorage.getItem('auth');
-
-    /**
-     * jeśli nie będzie kodu to znaczy że zaraz będziemy się uwierzytelniać
-     * ustawmy więc flagę auth = true w local storage
-     * zdejmiemy ją po pomyślnym ustawieniu auth_code
-     * w konstruktorze głównego komponentu będziemy sprawdzać tą flagę,
-     * jeśli =true to zawołamy getToken żeby z paska adresu zmaczowała auth_code
-     */
-    if(!code && !auth){
-      localStorage.setItem('auth', 'true');
+  users = [
+    {
+      email: "kamil.kowalczyk992@gmail.com",
+      name: "Kamil",
+      lastName: "Kowalczyk"
     }
+  ];
 
-
-    if(!code){
-      if(this.matchLocationToAuthCode(code)){
-        localStorage.setItem('auth', 'false');
-        this.changeAuthCodeToAccessToken(localStorage.getItem('auth_code'));
-        return;
-      }
-    }
-
-    if(!code){
-      this.requestAuthCode();
-    }
-
-    //TODO: ustawienie access tokena w headersach
-    //this.httpHeaders.set('Authorization', 'Bearer ' + token);
-
+  getUser(email){
+    var _user;
+    this.users.forEach(user=>{
+      if(user.email == email){
+        _user = user;
+      } 
+    })
+    return _user;
   }
 
-  matchLocationToAuthCode(code){
-    console.log("start matchLocationToAuthCode()");
-    var match = window.location.href.match(/\?code=(.*?)#/);
-    code = match && match[1];
-    if(match){
-      localStorage.setItem('auth_code', code);
-      return true;
-    }
-    return false;
+  addUser(user){
+    this.users.push(user);
   }
-
-  requestAuthCode(){
-    console.log("start requestAuthCode()");
-    localStorage.removeItem('auth_code');
-
-    let location = 'https://accounts.google.com/o/oauth2/v2/auth';
-    let redirect_uri = 'http://localhost:4200/';
-    let client_id = '660414676955-apq8kh280raba6dhuq003mu4oo5c57bp.apps.googleusercontent.com';
-    let prompt = 'consent';
-    let response_type = 'code';
-    let scope = 'https://www.googleapis.com/auth/userinfo.profile+https://www.googleapis.com/auth/userinfo.email';
-    let access_type = 'offline';
-
-    window.location.replace(`${location}?redirect_uri=${redirect_uri}&prompt=${prompt}&response_type=${response_type}&client_id=${client_id}&scope=${scope}&access_type=${access_type}`);
-    //UWAGA: to mi spowoduje ze bedziemy budowac caly modul od zera, bo przechodzimy na inna strone, a potem wracamy. Po tym kodzie funkcja wykonuje reszte instrukcji po czym robi redirecta
-  }
-
-  changeAuthCodeToAccessToken(_code){
-    console.log("start changeAuthCodeToAccessToken()");
-    let location = 'https://www.googleapis.com/oauth2/v4/token';
-    let code = _code;
-    let redirect_uri = 'http://localhost:4200/';
-    let client_id = '660414676955-apq8kh280raba6dhuq003mu4oo5c57bp.apps.googleusercontent.com';
-    let client_secret = 'ikckkvUK3Chl2COF46XghNmT';
-    let scope = '';
-    let grant_type = 'authorization_code';
-
-    let url = `${location}?code=${code}&redirect_uri=${redirect_uri}&client_id=${client_id}&client_secret=${client_secret}&scope=${scope}&grant_type=${grant_type}`;
-
-    const httpOptions = {
-      headers: new HttpHeaders({
-      })
-    };
-
-    this.http.post(url, httpOptions).toPromise()
-        .then(this.extractData)
-        .catch(this.handleErrorPromise);
-
-  }
-
-  private extractData(res){
-    localStorage.setItem('access_token', res.access_token);
-    localStorage.setItem('refresh_token', res.refresh_token);
-    localStorage.setItem('id_token', res.id_token);
-    localStorage.setItem('expires_in', res.expires_in);
-    localStorage.setItem('token_type', res.token_type);
-    //TODO: moze zapisac to w session storage??
-  }
-
-  private handleErrorPromise (error: Response | any) {
-    console.error(error.message || error);
-    return Promise.reject(error.message || error);
-  }
-
-  getGoogleData(access_token){
-    let url = 'https://www.googleapis.com/oauth2/v2/userinfo';
-    const httpOptions = {
-      headers: new HttpHeaders({
-        'Authorization' : 'Bearer ' + access_token
-      })
-    };
-
-    return this.http.get(url, httpOptions);
-  }
-
 }
--- web/src/app/sign-in/sign-in.component.ts
+++ web/src/app/sign-in/sign-in.component.ts
@@ -1,49 +1,58 @@
 import { Component, OnInit } from '@angular/core';
-import { AuthService } from './auth-service.service';
+import { AuthService } from "angular4-social-login";
+import { GoogleLoginProvider } from "angular4-social-login";
+import { SocialUser } from "angular4-social-login";
+import { logging } from 'protractor';
+import { UsersService } from './auth-service.service';
+
+
 
 @Component({
   selector: 'app-sign-in',
   template: `
-    <div class="row">
-      <button type="button" (click)="authorize()" class="btn btn-info">Zaloguj się z Google</button>
-         <div>
-    <h3 *ngIf="name">{{name}}</h3>
-    <h3 *ngIf="lastName">{{lastName}}</h3>
-    <h4 *ngIf="mail">{{mail}}</h4>
+  <div class="row btn-group">
+      <button *ngIf="!loggedIn" type="button" (click)="signInWithGoogle()" class="btn btn-info">Zaloguj się z Google</button>
+      <button *ngIf="loggedIn" type="button" (click)="signOut()" class="btn btn-info">Wyloguj się</button>
   </div>
-    </div>
+  <div *ngIf="userInfo" >
+    <h4>{{userInfo.name}} {{userInfo.lastName}}</h4>
+    <h5>{{userInfo.email}}</h5>
+  </div>
   `,
   styles: []
 })
 export class SignInComponent implements OnInit {
 
+  userInfo = {};
+
+  loggedIn;
+
+  constructor(private authService: AuthService, private userService: UsersService) {
+
+  }
 
 
-  constructor(private authService: AuthService) {
-    var auth = localStorage.getItem('auth');
-    if(auth){
-      this.authService.getToken();
+  signInWithGoogle(): void {
+    this.authService.signIn(GoogleLoginProvider.PROVIDER_ID);
+  }
 
-      var token = localStorage.getItem('access_token');
-
-      //TODO: w tym momencie nie przyszła jeszcze odpowiedź z POST-a, więc nie ma jeszcze access_token, gdyby udało się jakoś zaczekać na tą odpowiedź....
-      if(token){this.authService.getGoogleData(token).subscribe(data=>{
-        this.name = data.name;
-      })} else {
-
-      }
-
-
-    }}
-
-  authorize(){
-    this.authService.getToken();
+  signOut(): void {
+    this.authService.signOut();
+    this.loggedIn = false;
+    this.userInfo = {};
   }
 
   ngOnInit() {
-  }
+   setTimeout(()=>{
+    this.authService.authState.subscribe((user) => {
+      console.log("Trying to get user: " + user)
+      if(user){
+        this.userInfo = this.userService.getUser(user.email);
+      }
+      this.loggedIn = (user != null);
+    });
+   },5000); //TODO: działa, ale do poprawienia (routes!?) - trzeba jakoś poczekać na response
 
-  name ;
-  lastName;
-  mail;
+  }
+  
 }
