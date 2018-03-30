d14c88f65e65b3b50da5fae42569b9137fd216d3
kamil.kowalczyk <karmel92@o2.pl>
2018-03-30 14:16:40 +0200

    add email validation

--- server/src/main/java/pl/kjkow/server/model/User.java
+++ server/src/main/java/pl/kjkow/server/model/User.java
@@ -6,6 +6,7 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.validation.constraints.Email;
 import java.util.Date;
 
 /**
@@ -19,6 +20,7 @@ public class User {
     private Long id;
     private String firstName;
     private String lastName;
+    @Email
     private String email;
     @CreatedDate
     private Date created;
