7e2a6dc41074ad15927e4db826efc5608a6246dc
kamil.kowalczyk <karmel92@o2.pl>
2018-04-01 08:16:37 +0200

    przechodzi test get user by email

--- server/src/test/java/pl/kjkow/server/rest/UserRestTest.java
+++ server/src/test/java/pl/kjkow/server/rest/UserRestTest.java
@@ -1,55 +1,76 @@
 package pl.kjkow.server.rest;
 
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.web.context.WebApplicationContext;
+import pl.kjkow.server.ServerApplication;
 import pl.kjkow.server.model.User;
+import pl.kjkow.server.repository.UserRepository;
 
 import java.nio.charset.Charset;
+import java.util.ArrayList;
+import java.util.List;
 
-import static org.junit.Assert.*;
-import static org.mockito.BDDMockito.given;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import static org.hamcrest.Matchers.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import static org.springframework.test.web.servlet.setup.MockMvcBuilders.*;
 
 
 /**
  * Created by kamil on 2018-03-31.
  */
 @RunWith(SpringRunner.class)
-@WebMvcTest(UserRest.class)
+@SpringBootTest(classes = ServerApplication.class)
+@WebAppConfiguration
 public class UserRestTest {
 
     private MediaType contentType = new MediaType(MediaType.APPLICATION_JSON.getType(),
             MediaType.APPLICATION_JSON.getSubtype(),
             Charset.forName("utf8"));
 
-    @Autowired
+    private List<User> userList;
+
+    private String email = "doit@yourself.com";
+
     private MockMvc mvc;
 
-    @MockBean
-    private UserRest userRest;
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private WebApplicationContext webApplicationContext;
+
+    @Before
+    public void setUp(){
+        this.mvc = webAppContextSetup(webApplicationContext).build();
+        userList = new ArrayList<>();
+
+        userList.add(userRepository.save(new User("John", "Doe", "abc@example.com")));
+        userList.add(userRepository.save(new User("Michelle", "Francis", email)));
+    }
 
     @Test
     public void getUserByEmail() throws Exception {
-        String email = "doit@yourself.com";
-        User user = new User();
-        user.setEmail(email);
-
-        given(userRest.getUserByEmail(email)).willReturn(user);
-
-/*        mvc.perform(get("/users/" + email))
-                .andExpect(status().isFound())
-                .andExpect(content().contentType(contentType))*/
-//https://spring.io/guides/tutorials/bookmarks/ TODO: dokonczyc (ustawianie listy users oraz wstrzykniecie UserRepository
+        mvc.perform(get("/users/" + email))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(contentType))
+                .andExpect(jsonPath("$.firstName", is(userList.get(1).getFirstName())))
+                .andExpect(jsonPath("$.lastName", is(userList.get(1).getLastName())))
+                .andExpect(jsonPath("$.email", is(userList.get(1).getEmail())));
 
     }
+
+    //TODO: dodac test wyjatku usernotfoundexception, https://spring.io/guides/tutorials/bookmarks/
 
     @Test
     public void addUser() throws Exception {
