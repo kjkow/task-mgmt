283e458d6d64fc411bf694eb65041eaadc51d8e8
kamil.kowalczyk <kamil.kowalczyk@asseco.pl>
2018-04-03 14:56:55 +0200

    nie rejestruj od razu uzytkownika jesli nie ma go na bazie, dopiero jak potwierdzi

--- web/src/app/sign-in/sign-in.component.ts
+++ web/src/app/sign-in/sign-in.component.ts
@@ -21,19 +21,36 @@ import { User } from './customer';
     <p>{{userInfo.firstName}} {{userInfo.lastName}} <br> {{userInfo.email}}</p>
     </div>
   </div>
+  <div *ngIf="userNotFound">
+    <h5>Utworzyć nowe konto dla adresu e-mail {{emailAddress}}?</h5>
+    <button type="button" (click)="register()" class="btn btn-info">Tak</button>
+  </div>
   `,//TODO: zrobić ładny html
   styles: []
 })
+//TODO: za dużo flag, miesza się to wszystko trzeba to rozbić
 export class SignInComponent implements OnInit {
 
   userInfo: User;
 
   loggedIn: boolean;
 
+  emailAddress;
+  userNotFound;
+  socialUser;
+
   constructor(private authService: AuthService, private userService: UsersService) {
 
   }
 
+  register(){
+    this.userService.addUser(this.socialUser).subscribe(u =>{
+      this.userInfo.email = u.email;
+      this.userInfo.firstName = u.firstName;
+      this.userInfo.lastName = u.lastName;
+      this.loggedIn = true;
+    })
+  }
 
   signInWithGoogle(): void {
     this.authService.signIn(GoogleLoginProvider.PROVIDER_ID);
@@ -42,6 +59,7 @@ export class SignInComponent implements OnInit {
   signOut(): void {
     this.authService.signOut();
     this.loggedIn = false;
+    this.userNotFound = false;
     this.userInfo.email = "";
     this.userInfo.firstName = "";
     this.userInfo.lastName = "";
@@ -50,9 +68,13 @@ export class SignInComponent implements OnInit {
   ngOnInit() {
     this.userInfo = new User;
     this.loggedIn = false;
+    this.userNotFound = false;
+
     setTimeout(()=>{
       this.authService.authState.subscribe((user) => {
         if(user){
+          this.emailAddress = user.email;
+          this.socialUser = user;
           this.userService.getUser(user).subscribe(u =>{
             if(u){
               this.userInfo.email = u.email;
@@ -62,12 +84,7 @@ export class SignInComponent implements OnInit {
             }
         }, err => {
             if(err.error.status == 404){
-                this.userService.addUser(user).subscribe(u =>{
-                this.userInfo.email = u.email;
-                this.userInfo.firstName = u.firstName;
-                this.userInfo.lastName = u.lastName;
-                this.loggedIn = true;
-                })
+                this.userNotFound = true;
           }
         })
       }
