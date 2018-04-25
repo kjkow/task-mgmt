da310eb0cec544a706e35520fe15a0da5d41244e
kamil.kowalczyk <kamil.kowalczyk@asseco.pl>
2018-04-25 12:18:14 +0200

    dodaj wyswietlanie adresu email

--- web/src/app/account-settings/user-settings/user-settings.component.ts
+++ web/src/app/account-settings/user-settings/user-settings.component.ts
@@ -6,19 +6,25 @@ import { Component, OnInit } from '@angular/core';
   <div class="card">
     <h5 class="card-title">Ustawienia konta</h5>
     <form>
+      <div class="form-group row">
+        <label for="staticEmail" class="col-sm-2 col-form-label">Email</label>
+        <div class="col-sm-10">
+          <input type="text" readonly class="form-control-plaintext" id="staticEmail" value="email@example.com">
+        </div>
+      </div>
       <div class="form-group">
         <label for="userName">Nazwa użytkownika</label>
         <input id="userName" type="text" name="name" class="form-control" maxlength="20">
       </div>
-    <div class="form-group form-check">
-      <input class="form-check-input" id="emailNotifications" type="checkbox" name="notifications">
-      <label class="form-check-label" for="emailNotifications">Włączone powiadomienia e-mail</label>
-    </div>
-    <div class="form-group">
-      <label for="daysBeforeNotify">Ile dni przed terminem wysłać powiadomienie</label>
-      <input id="daysBeforeNotify" type="number" name="daysNotify" class="form-control">
-    </div>
-    <button type="submit" class="btn btn-primary">Zapisz</button>
+      <div class="form-group form-check">
+        <input class="form-check-input" id="emailNotifications" type="checkbox" name="notifications">
+        <label class="form-check-label" for="emailNotifications">Włączone powiadomienia e-mail</label>
+      </div>
+      <div class="form-group">
+        <label for="daysBeforeNotify">Ile dni przed terminem wysłać powiadomienie</label>
+        <input id="daysBeforeNotify" type="number" name="daysNotify" class="form-control">
+      </div>
+      <button type="submit" class="btn btn-primary">Zapisz</button>
     </form>
   </div>
   `,
