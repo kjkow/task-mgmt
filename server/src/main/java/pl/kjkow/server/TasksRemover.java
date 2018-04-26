f12ed4984c85f15c31287f3d81313fde371359d8
kamil.kowalczyk <kamil.kowalczyk@asseco.pl>
2018-04-26 10:45:50 +0200

    implementacja zadania powtarzalnego

--- server/src/main/java/pl/kjkow/server/TasksRemover.java
+++ server/src/main/java/pl/kjkow/server/TasksRemover.java
@@ -2,8 +2,14 @@ package pl.kjkow.server;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
+import pl.kjkow.server.model.Area;
+import pl.kjkow.server.repository.TaskRepository;
+
+import java.util.Calendar;
+import java.util.Date;
 
 /**
  * Created by Kamil.Kowalczyk on 2018-04-25.
@@ -15,9 +21,15 @@ public class TasksRemover {
 
     private static final Logger log = LoggerFactory.getLogger(TasksRemover.class);
 
+    @Autowired
+    private TaskRepository taskRepository;
+
     @Scheduled(cron = "0 0 3 1 * *") //pierwszego dnia każdego miesiąca o 3 w nocy
     public void removeFinnishedTasksOlderThanMonth() {
-        //TODO: mniej wiecej mam to napisane w tescie repozytorium
+        Calendar cal = Calendar.getInstance();
+        cal.add(Calendar.DAY_OF_MONTH, -30);
+        Date thirtyDaysAgo = cal.getTime();
 
+        taskRepository.deleteByFinnishedBeforeAndArea(thirtyDaysAgo, Area.UKONCZONE);
     }
 }
