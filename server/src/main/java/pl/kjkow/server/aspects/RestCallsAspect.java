39193878836ac0ba95103d9904e528abee2e4e51
kamil.kowalczyk <kamil.kowalczyk@asseco.pl>
2018-05-07 15:52:54 +0200

    refaktor przechwytywania zadan rest

--- server/src/main/java/pl/kjkow/server/aspects/RestCallsAspect.java
+++ server/src/main/java/pl/kjkow/server/aspects/RestCallsAspect.java
@@ -1,9 +1,7 @@
 package pl.kjkow.server.aspects;
 
 import org.aspectj.lang.JoinPoint;
-import org.aspectj.lang.annotation.After;
-import org.aspectj.lang.annotation.Aspect;
-import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.*;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Configuration;
@@ -20,28 +18,44 @@ public class RestCallsAspect {
     private static final Logger log = LoggerFactory.getLogger(RestCallsAspect.class);
     private UUID uuid;
 
-    @Before("execution(* pl.kjkow.server.rest.*.*(..))")
+    @Pointcut("execution(* pl.kjkow.server.rest.*.*(..))")
+    public void restPackage(){}
+
+    @Before("restPackage()")
     private void beforeMethod(JoinPoint joinPoint){
         uuid = UUID.randomUUID();
-        log.info(createLogInformation(joinPoint, "ZAPYTANIE DO SERWERA "));
+        log.info(createRequestLogInformation(joinPoint));
     }
 
-    @After("execution(* pl.kjkow.server.rest.*.*(..))")
-    private void afterMethod(JoinPoint joinPoint){
-        log.info(createLogInformation(joinPoint, "ODPOWIEDZ Z SERWERA "));
+    @AfterReturning(pointcut = "restPackage()", returning = "returnValue")
+    public void afterOkResponse(Object returnValue){
+        log.info(createResponseLogInformation(returnValue));
     }
 
-    private String createLogInformation(JoinPoint joinPoint, String kierunek){
-        StringBuilder stringBuilder = new StringBuilder();
-        stringBuilder
-                .append(kierunek)
-                .append(joinPoint.getSignature().toShortString())
-                .append(" ")
-                .append(uuid);
+    @AfterThrowing(throwing = "e", pointcut = "restPackage()")
+    public void afterExceptionResponse(Exception e){
+        log.info(createResponseLogInformation(e.getMessage()));
+    }
+
+    private String createRequestLogInformation(JoinPoint joinPoint){
+        StringBuilder sb = new StringBuilder();
+            sb.append("Client requested ")
+            .append(joinPoint.getSignature().toShortString())
+            .append(" ")
+            .append(uuid)
+            .append(" with parameters:");
         for(Object o : joinPoint.getArgs()){
-            stringBuilder.append("\n");
-            stringBuilder.append(o.toString());
+            sb.append("\n").append(o.toString());
         }
-        return stringBuilder.toString();
+        return sb.toString();
+    }
+
+    private String createResponseLogInformation(Object returnedValue){
+        return "Server responded " +
+                " " +
+                uuid +
+                " with:" +
+                "\n" +
+                returnedValue.toString();
     }
 }
