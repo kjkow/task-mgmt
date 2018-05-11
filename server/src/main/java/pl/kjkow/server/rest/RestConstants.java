356e1884fd74eaafdd5341f991975761b953f8c1
kamil.kowalczyk <karmel92@o2.pl>
2018-05-11 12:55:34 +0200

    porzadki w user rest

--- server/src/main/java/pl/kjkow/server/rest/RestConstants.java
+++ server/src/main/java/pl/kjkow/server/rest/RestConstants.java
@@ -9,4 +9,8 @@ class RestConstants {
     static final String ADD_TASK = "/tasks/add/";
     static final String GET_ALL_TASKS = "/tasks/{id}";
     static final String UPDATE_TASK = "tasks/update/{taskId}";
+
+    static final String GET_USER_BY_ID = "/users/{id}/{email}";
+    static final String ADD_USER = "users/add";
+    static final String UPDATE_USER_DATA = "users/{id}";
 }
