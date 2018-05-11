ef98ec4209f112719430e3dfcb6d6e92ca194a1a
kamil.kowalczyk <karmel92@o2.pl>
2018-05-11 15:33:59 +0200

    test pobrania uzytkownika

--- server/src/test/java/pl/kjkow/server/rest/UserRestTest.java
+++ server/src/test/java/pl/kjkow/server/rest/UserRestTest.java
@@ -1,11 +1,30 @@
 package pl.kjkow.server.rest;
 
 import org.junit.Test;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import pl.kjkow.server.model.User;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
 
 public class UserRestTest extends RestTest {
 
     @Test
     public void getUserById() {
+        ResponseEntity<User> response = restTemplate.exchange(
+                "/users/123456/testing@example.com",
+                HttpMethod.GET,
+                new HttpEntity<>(getAuthenticationHeaders()),
+                User.class);
+        User recived = response.getBody();
+
+        assertEquals(HttpStatus.OK, response.getStatusCode());
+        assertEquals(recived.getName(), "John Tester");
+        assertEquals(recived.getEmail(), "testing@example.com");
+        assertFalse(recived.isNotifications());
     }
 
     @Test
