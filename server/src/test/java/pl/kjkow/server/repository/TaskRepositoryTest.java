f1d6ac68b1bf6f1a7babf019ae74eb4d7838b126
kamil.kowalczyk <karmel92@o2.pl>
2018-05-11 17:55:32 +0200

    uporzadkowanie testow

--- server/src/test/java/pl/kjkow/server/repository/TaskRepositoryTest.java
+++ server/src/test/java/pl/kjkow/server/repository/TaskRepositoryTest.java
@@ -1,4 +1,4 @@
-package pl.kjkow.server;
+package pl.kjkow.server.repository;
 
 import org.junit.After;
 import org.junit.Before;
@@ -11,11 +11,9 @@ import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
 import org.springframework.test.context.junit4.SpringRunner;
 import pl.kjkow.server.model.Area;
 import pl.kjkow.server.model.Task;
-import pl.kjkow.server.repository.TaskRepository;
 
 import java.util.Calendar;
 import java.util.Date;
-import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -88,6 +86,7 @@ public class TaskRepositoryTest {
         cal.add(Calendar.DAY_OF_MONTH, -30);
         Date thirtyDaysAgo = cal.getTime();
 
+        //TODO: restult of assert that is ignored
         assertThat(taskRepository.findById(1L).isPresent());
 
         taskRepository.deleteByFinnishedBeforeAndArea(thirtyDaysAgo, Area.UKONCZONE);
