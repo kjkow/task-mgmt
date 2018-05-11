2537bc30212d0464d41a1eade0e876d832d3de92
kamil.kowalczyk <karmel92@o2.pl>
2018-05-11 06:45:45 +0200

    ukoncz test getAllProjects, dodaj javadoc

--- server/src/test/java/pl/kjkow/server/rest/ProjectRestTest.java
+++ server/src/test/java/pl/kjkow/server/rest/ProjectRestTest.java
@@ -4,18 +4,29 @@ package pl.kjkow.server.rest;
 import org.junit.Test;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 
+import java.util.*;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+
+/**
+ * Strongly coupled with
+ * @see pl.kjkow.server.repository.DataFillerOnStartup
+ */
 public class ProjectRestTest extends RestTest {
-
 
     @Test
     public void getAllProjects() {
-        ResponseEntity<Object> response = restTemplate.exchange(
-                RestConstants.GET_ALL_PROJECTS, HttpMethod.GET, new HttpEntity<>(getAuthenticationHeaders()), Object.class);
-        System.out.println(response);
-
+        ResponseEntity<ArrayList> response = restTemplate.exchange(
+                RestConstants.GET_ALL_PROJECTS, HttpMethod.GET, new HttpEntity<>(getAuthenticationHeaders()), ArrayList.class);
+        assertNotNull(response.getBody());
+        assertEquals(HttpStatus.OK, response.getStatusCode());
+        assertTrue(response.getBody().size() > 0);
+        assertEquals(((LinkedHashMap)response.getBody().get(0)).get("description"), "This is project for testing purposes");
     }
 
     @Test
