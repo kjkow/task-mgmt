c89ff0b86af57c2f9abec1be4295028db1353347
kamil.kowalczyk <karmel92@o2.pl>
2018-05-12 11:58:13 +0200

    refaktor

--- server/src/test/java/pl/kjkow/server/services/TaskServiceTest.java
+++ server/src/test/java/pl/kjkow/server/services/TaskServiceTest.java
@@ -1,13 +1,11 @@
 package pl.kjkow.server.services;
 
-import org.junit.Before;
+import org.junit.Rule;
 import org.junit.Test;
-import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.mockito.junit.MockitoJUnitRunner;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.test.context.ContextConfiguration;
+import org.mockito.junit.MockitoJUnit;
+import org.mockito.junit.MockitoRule;
 import pl.kjkow.server.model.Area;
 import pl.kjkow.server.model.Task;
 import pl.kjkow.server.repository.TaskRepository;
@@ -21,52 +19,22 @@ import static org.mockito.Mockito.when;
 /**
  * Created by kamil on 2018-04-27.
  */
-@RunWith(MockitoJUnitRunner.class)
-@ContextConfiguration(value = "application-dev.properties")
 public class TaskServiceTest {
 
-    @Mock
-    private TaskRepository mockRepository;
+    @Rule public MockitoRule mockitoRule = MockitoJUnit.rule();
 
-    @Value("${task.limit}")
-    private int limit;
-
-    @InjectMocks
-    private TaskService taskService;
-
-    @Before
-    public void init(){
-        testTaskOne = new Task("TaskOne", Area.MOZE_KIEDYS, "123");
-        testTaskTwo = new Task("Other", Area.MOZE_KIEDYS, "123");
-        testTaskThree = new Task("Test", Area.MOZE_KIEDYS, "124");
-        testTaskFour = new Task("Test", Area.OBOWIAZKI, "123");
-    }
-
-    private Task testTaskOne;
-    private Task testTaskTwo;
-    private Task testTaskThree;
-    private Task testTaskFour;
+    @Mock private TaskRepository mockRepository;
+    @InjectMocks private TaskService taskService;
 
     @Test
-    public void save() throws Exception {
-
-    }
-
-    @Test
-    public void findAll() throws Exception {
-        List<Task> tasks = Arrays.asList(testTaskOne, testTaskTwo, testTaskFour);
+    public void findAll() {
+        List<Task> tasks = Arrays.asList(
+                new Task("TaskOne", Area.MOZE_KIEDYS, "123"),
+                new Task("Other", Area.MOZE_KIEDYS, "123"),
+                new Task("Test", Area.OBOWIAZKI, "123"));
         when(mockRepository.findByUserId("123")).thenReturn(tasks);
         List<Task> actual = taskService.findAll("123");
         assertEquals(actual, tasks);
-    }
-
-    @Test
-    public void updateTask() throws Exception {
-    }
-
-    @Test()
-    public void sectionSetInWrongArea(){
-        //todo: mockito wstawia w taskservice#limit wartosc 0, a nie chce brac jej z application-dev.properties
     }
 
 }
