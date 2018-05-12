46634681441188a1b781905e6905b979353ead6d
kamil.kowalczyk <karmel92@o2.pl>
2018-05-12 11:43:28 +0200

    aspekt test

--- server/src/test/java/pl/kjkow/server/aspects/RestCallsAspectTest.java
+++ server/src/test/java/pl/kjkow/server/aspects/RestCallsAspectTest.java
@@ -1,29 +1,64 @@
 package pl.kjkow.server.aspects;
 
 import org.aspectj.lang.JoinPoint;
-import org.aspectj.lang.ProceedingJoinPoint;
-import org.aspectj.lang.annotation.Pointcut;
+import org.aspectj.lang.Signature;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.ExpectedException;
 import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnit;
+import org.mockito.junit.MockitoRule;
 
-import java.lang.reflect.Method;
-
-import static com.sun.javaws.JnlpxArgs.verify;
-import static org.junit.Assert.*;
-import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.*;
 
 public class RestCallsAspectTest {
+
+    @Rule
+    public ExpectedException expectedEx = ExpectedException.none();
+
+    @Rule
+    public MockitoRule mockitoRule = MockitoJUnit.rule();
 
     @Mock
     private JoinPoint joinPoint;
 
-    @Test
-    public void afterOkResponse() {
-        //verify(joinPoint, times(1))
-        //https://stackoverflow.com/questions/41389015/junit-tests-for-aspectj
+    private RestCallsAspect restCallsAspect = new RestCallsAspect();
+
+    @Before
+    public void init(){
+        Signature mockedSignature = mock(Signature.class);
+        when(joinPoint.getSignature()).thenReturn(mockedSignature);
     }
 
     @Test
-    public void afterExceptionResponse() {
+    public void missingAuthParameters() {
+        Object[] args = {"parameter"};
+        when(joinPoint.getArgs()).thenReturn(args);
+
+        expectedEx.expect(RuntimeException.class);
+        expectedEx.expectMessage("Missing authentication parameters");
+        restCallsAspect.beforeMethod(joinPoint);
+    }
+
+    @Test
+    public void firstArgMismatch(){
+        Object[] args = {5, "parameter"};
+        when(joinPoint.getArgs()).thenReturn(args);
+
+        expectedEx.expect(RuntimeException.class);
+        expectedEx.expectMessage("Argument type mismatch. First should be String");
+        restCallsAspect.beforeMethod(joinPoint);
+    }
+
+    @Test
+    public void secondArgMismatch(){
+        Object[] args = {"parameter", 5};
+        when(joinPoint.getArgs()).thenReturn(args);
+
+        expectedEx.expect(RuntimeException.class);
+        expectedEx.expectMessage("Argument type mismatch. Second should be String");
+        restCallsAspect.beforeMethod(joinPoint);
     }
 }
